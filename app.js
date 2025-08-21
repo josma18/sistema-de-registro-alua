f0e3bab53fca99610a2a2bce301e09c6371a32bd
Jose Diaz <josemanueldiazc20@gmail.com>
2025-08-21 00:00:05 -0500

    Actualizar app.js e index.html

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 //Se creo un .js desde Github
-console.log("Ejecutando el sistema");
+let nombre = prompt("Ingrese nombre de Usuario:");
+let sexo = prompt("Ingrese el sexo: M (masculino)/F (femenino)/ NB (no binario)");
+
+function Recibimiento(elemento,texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+};
+
+if (sexo == "M") {
+    Recibimiento("h1",`Sea Bienvenido a la pagina de practica de Git y Github, Alumno ${nombre}`);
+} else if (sexo == "F"){
+    Recibimiento("h1",`Sea Bienvenido a la pagina de practica de Git y Github, Alumna ${nombre}`);
+} else {
+    Recibimiento("h1",`Sea Bienvenido a la pagina de practica de Git y Github, ${nombre}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
